9d3ee85224ab4b45e3c09e0e68adb43eee16b916
Dave Hirschfeld <david.hirschfeld@stanwell.com>
2019-01-31 15:52:26 +1000

    Fix post-link.bat

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,3 +1,4 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable qgrid --py --sys-prefix > NUL 2>&1 && if errorlevel 1 exit 1
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable qgrid --py --sys-prefix
+if %ERRORLEVEL% neq 0 exit 1
