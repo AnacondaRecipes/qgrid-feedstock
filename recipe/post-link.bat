474d9d03b97f8e3cdbee9b8ad0ff8d8da98b3e75
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2019-02-06 10:21:51 +1000

    Really fix post-link.bat

--- recipe/post-link.bat
+++ recipe/post-link.bat
@@ -1,4 +1,5 @@
 @echo off
 
-"%PREFIX%\Scripts\jupyter-nbextension.exe" enable qgrid --py --sys-prefix
-if %ERRORLEVEL% neq 0 exit 1
+:: Set PATH explicitly as it may not be set correctly by some versions of conda
+set "PATH=%PATH%;%PREFIX%\Library\bin"
+"%PREFIX%\Scripts\jupyter-nbextension.exe" enable qgrid --py --prefix %PREFIX% >>"%PREFIX%\.messages.txt" 2>&1 && if errorlevel 1 exit 1
